18c1c7fc3aca3e8f96521fe72fbdda9697070e1c
Sabin <sabintheworld@gmail.com>
2020-02-03 12:21:59 -0800

    Having too much fun adding animations

--- src/components/RandomIcon/RandomIcon.js
+++ src/components/RandomIcon/RandomIcon.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import logo from '../../assets/images/logo.svg'
 import './RandomIcon.scss'
-import { random } from 'lodash'
+import { random, merge } from 'lodash'
 
 function RandomIcon() {
+
+    const logoStyleVars = {
+        height: random(1,10),
+        left: random(1,90),
+        top: random(1,90)
+    }
 
     const animations = [
         `floaty infinite ${ random(4,8) }s ease-in-out`,
         `spinny infinite ${ random(10,20) }s linear`
     ]
 
+    if ( logoStyleVars.height < 4 ) {
+        animations.push(`scaly infinite ${ random(4,8)}s ease-in-out`)
+    }
+
     const logoStyle = {
-        height: random(1,10) + 'vmin',
-        left: random(1,90) + '%',
-        top: random(1,90) + '%',
+        height: `${logoStyleVars.height}vmin`,
+        left: `${logoStyleVars.left}%`,
+        top: `${logoStyleVars.top}%`,
         animation: animations[random(0, animations.length - 1)]
     }
 
